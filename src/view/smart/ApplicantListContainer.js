33861b38817fda17f5e2cfd61d4b0387cafd75ae
John Snelgrove <johnny.snelgrove@gmail.com>
2018-04-22 15:13:43 -0700

    wire up smart components to store

--- src/view/smart/ApplicantListContainer.js
+++ src/view/smart/ApplicantListContainer.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { filteredApplicants } from '../../state/selectors'
+import ApplicantListContainerView from '../dumb/ApplicantListContainerView'
 
 const mapStateToProps = (state, ownProps) => ({
   applicants: filteredApplicants(state),
 })
 
-class ApplicantListContainer extends Component {
-  render() {
-    return <div>{JSON.stringify(this.props)}</div>
-  }
-}
-
-export default connect(mapStateToProps)(ApplicantListContainer)
+export default connect(mapStateToProps)(ApplicantListContainerView)
--- src/view/smart/TagsContainer.js
+++ src/view/smart/TagsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { tags, selectedTags, selectedTagCount } from '../../state/selectors'
 import { addSelectedTag, removeSelectedTag, clearSelectedTags } from '../../state/actions'
+import TagsContainerView from '../dumb/TagsContainerView'
 
 const mapStateToProps = (state, ownProps) => ({
   tags: tags(state),
@@ -15,10 +16,4 @@ const mapDispatchToProps = {
   clearSelectedTags,
 }
 
-class TagsContainer extends Component {
-  render() {
-    return <div>{JSON.stringify(this.props)}</div>
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(TagsContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(TagsContainerView)
