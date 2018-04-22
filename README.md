3ac3528b4d706ee455edaa16c5d2e720b25482fa
John Snelgrove <johnny.snelgrove@gmail.com>
2018-04-21 17:37:36 -0700

    init react skeleton

---
+++ README.md
@@ -0,0 +1,20 @@
+# My App
+
+## Description
+
+My cool app!
+
+## Setup
+
+```bash
+npm install
+npm start
+```
+
+Open [localhost:1234](localhost:1234) in a browser.
+
+## Testing
+
+```bash
+npm test
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => (
+  <div className="App">
+    <h1 className="App-Title">Hello Parcel x React</h1>
+  </div>
+)
+
+render(<App />, document.getElementById('root'))
