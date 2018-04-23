143d4a670d12f92b3ea7afdef1824cd9110e9b11
John Snelgrove <johnny.snelgrove@gmail.com>
2018-04-22 19:48:38 -0700

    styled applicant list

--- src/view/dumb/ApplicantListContainerView.js
+++ src/view/dumb/ApplicantListContainerView.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Avatar } from 'rebass'
 import { Col, Row, raised, borderRadius, verticalScroll } from './common/'
 import upperfirst from 'lodash.upperfirst'
 
@@ -13,40 +12,67 @@ const capitalize = (name) => {
 
 const ListContainer = styled.div`
   ${verticalScroll};
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px 20px 20px 20px;
+  background-color: #efefef;
 `
 
 const Card = styled.div`
   ${raised};
   ${borderRadius};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+  font-family: helvetica
   width: 100%;
-  height: 100px;
   margin: 15px 0;
+  max-width: 700px;
   & > div:first-child {
-    align-items: center;
-    flex: 0 0 33%;
     display: flex;
-    h1 {
-      color: green;
+    flex-direction: row;
+    align-items: center;
+    border-bottom: 1px solid #d4d4d4;
+    padding: 15px;
+    background-color: white;
+
+    & > div:first-child {
+      img {
+        border-radius: 5px;
+        width: 65px;
+        height: 65px;
+        border: 2px solid #085ff7;
+      }
     }
-    img {
-      margin: 5%;
+    & > div:last-child {
+      padding-left: 15px;
+      h1:first-child {
+        color: #4c4c4c;
+        font-size: 21px;
+        padding-bottom: 8px;
+      }
+      h1 {
+        color: #a7a6a6;
+        font-size: 15px;
+        padding-bottom: 5px;
+      }
     }
   }
 
   & > div:last-child {
-    flex-flow: row wrap;
+    background-color: #f7f7f7;
+    padding: 15px 15px 5px 15px;
+    flex-direction: row;
+    flex-wrap: wrap;
     display: flex;
-    background: indianred;
   }
 `
 
 const Chip = styled.div`
-  ${raised};
+  box-shadow: 3px 3px 7px 0px hsla(218, 97%, 41%, 0.65);
   ${borderRadius};
+  background-color: #085ff7;
+  color: white;
+  padding: 15px;
+  margin: 0 10px 10px 0;
+  font-size: 13px;
 `
 
 const ApplicantListContainerView = ({ applicants }) => {
@@ -56,13 +82,16 @@ const ApplicantListContainerView = ({ applicants }) => {
         return (
           <Card key={a.email}>
             <Col>
-              <Avatar src={a.picture.large} />
-              <h1>{capitalize(a.name.first)}</h1>
-              <h1>{capitalize(a.name.last)}</h1>
+              <Col>
+                <img src={a.picture.large} />
+              </Col>
+              <Col>
+                <h1>{capitalize(a.name.first) + ' ' + capitalize(a.name.last)}</h1>
+                <h1>{'E: ' + a.email}</h1>
+                <h1>{'P: ' + a.phone}</h1>
+              </Col>
             </Col>
-            <Col>
-              <Row>{a.tags.map((t) => <Chip key={t}>{t}</Chip>)}</Row>
-            </Col>
+            <Col>{a.tags.map((t) => <Chip key={t}>{t}</Chip>)}</Col>
           </Card>
         )
       })}
